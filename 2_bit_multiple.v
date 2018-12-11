23e9ce3afb0f3524893cd1d18998af37beacf648
U-LAPTOP-LMNFABMS\Sandy <helloworld>
2018-12-11 11:18:17 +0800

    Dff

--- 2_bit_multiple.v
+++ 2_bit_multiple.v
@@ -55,8 +55,8 @@ module hex_7seg(hex_digit,seg);
 
 	always @ (hex_digit)
 	case (hex_digit)
-			4'h0: seg = ~7'h3F;
-			4'h1: seg = ~7'h06;     //   ---a----
+		4'h0: seg = ~7'h3F;
+		4'h1: seg = ~7'h06;     //   ---a----
 	        4'h2: seg = ~7'h5B;     // |           |
 	        4'h3: seg = ~7'h4F;     // f          b
 	        4'h4: seg = ~7'h66;     // |           |
@@ -65,10 +65,10 @@ module hex_7seg(hex_digit,seg);
 	        4'h7: seg = ~7'h07;     // e          c
 	        4'h8: seg = ~7'h7F;     // |           |
 	        4'h9: seg = ~7'h67;     //   ---d----
-		    4'hA: seg = ~7'h77;
-			4'hC: seg = ~7'h39;
-			4'hE: seg = ~7'h79;
-			4'hF: seg = ~7'h71;
+		4'hA: seg = ~7'h77;
+		4'hC: seg = ~7'h39;
+		4'hE: seg = ~7'h79;
+		4'hF: seg = ~7'h71;
         endcase
 endmodule
 
